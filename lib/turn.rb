cf7dab2f8e0a6c35ccbc8ba51670f0a100b68516
william george <williameverettgeorge@icloud.com>
2018-09-30 16:08:00 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,9 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+def input_to_index(input)
+	input.to_i - 1
+end
+def move(board, index, playerChar = "X")
+  board[index] = playerChar
+end
